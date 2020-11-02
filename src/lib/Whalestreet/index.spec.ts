3fe2390ed300b39fb0ab48f7e67d6779620dfd3c
alonavns <alona.vns@outlook.com>
2020-11-02 17:22:29 +0200

    spec restore

---
+++ src/lib/Whalestreet/index.spec.ts
@@ -0,0 +1,15 @@
+// tslint:disable:no-expression-statement
+import test from 'ava';
+
+test('contructor', async t => {
+  const addresses = {
+    FRight: '0xFc248D053E8E5F71542c0F4956f0292453393A87',
+    IRight: '0xdb210A5da035d160c7528BeCc349d58156818E7C',
+    RightsDao: '0xa43F7069C723587dedaC7c3c82C2f913a1806ff2'
+  };
+  t.deepEqual(addresses, {
+    FRight: '0xFc248D053E8E5F71542c0F4956f0292453393A87',
+    IRight: '0xdb210A5da035d160c7528BeCc349d58156818E7C',
+    RightsDao: '0xa43F7069C723587dedaC7c3c82C2f913a1806ff2'
+  });
+});
