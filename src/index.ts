54a2a8f9f10f6ec3962ba515dbd598642fc3aa74
alonavns <alona.vns@outlook.com>
2021-06-16 08:13:49 +0300

    [feat] - new version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import WhalestreetJS from './lib';
+export * as WhalestreetJSv1 from './lib/v1';
 
 export default WhalestreetJS;
---
+++ src/lib/v1.ts
@@ -0,0 +1,11 @@
+import Auctions from './Auctions/_index';
+import B20 from './B20/_index';
+import Farming from './Farming/_index';
+
+const library: any = {
+  Auctions,
+  B20,
+  Farming,
+};
+
+export default library;
